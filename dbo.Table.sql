165cb2a1e9e742e8fbf02be13d2eb42c2914c835
Matthew Agosto <matthewragosto@gmail.com>
2020-03-09 00:25:14 -0400

    Add sql files
    added create and stored proc scripts

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Date] INT NOT NULL,
+	[number] INT NOT NULL,
+	[company] TEXT NOT NULL,
+	[amount] FLOAT NOT NULL,
+	[paid] BIT NOT NULL, 
+
+
+)
---
+++ dbo.Update.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[Update]
+	@number int,
+	@paid bit
+AS
+BEGIN 
+	UPDATE Orders Set [paid] = @paid
+	WHERE number = @number
+END
